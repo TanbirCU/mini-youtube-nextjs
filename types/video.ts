8e791148895755d6fe7e20cf6ebca28f91e87061
TanbirCU <103535435+TanbirCU@users.noreply.github.com>
2026-09-10 16:35:02 +0600

    all new folders create

---
+++ types/video.ts
@@ -0,0 +1,15 @@
+export interface Video {
+  id: number;
+  title: string;
+  channelId: number;
+  channelName: string;
+  channelAvatar: string;
+  thumbnail: string;
+  duration: string;
+  views: number;
+  uploadedAt: string;
+  category: string;
+  description: string;
+  likes: number;
+  comments: number;
+}
